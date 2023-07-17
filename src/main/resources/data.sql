f323d1e3f218c7421a2d6d23defc6b41ed312b78
Pedro Henrique <pedrohcamargon@gmail.com>
2023-07-17 14:07:09 -0300

    teste de nomes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -28,3 +28,6 @@ INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_tra
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (4,'2019-08-07 08:12:45+03',-530.50,'SAQUE', null,2);
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (5,'2020-06-08 10:15:01+03',3241.23,'TRANSFERENCIA', 'Beltrano',1);
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (6,'2021-04-01 12:12:04+03',25173.09,'TRANSFERENCIA', 'Ronnyscley',2);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (7,'2022-10-31 12:12:04+03',7612.27,'SAQUE', 'Alberto',2);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (8,'2022-12-15 12:12:04+03',4889.69,'TRANSFERENCIA', 'Pedro',2);
+INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (9,'2023-01-21 12:12:04+03',15630.54,'DEPOSITO', 'Jose',2);
